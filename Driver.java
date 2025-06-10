2f8fc67041edaba62c38a97e5467e55b22f3845b
Lillian Thereault <lillian05ad@gmail.com>
2025-06-10 09:04:40 -0400

    Add files via upload
    Testing Driver class with three users and one admin account

--- Driver.java
+++ Driver.java
@@ -13,17 +13,41 @@ public class Driver {
     /** 2D Array of all accounts attached to a bank/ATM **/
     private ArrayList<Account> bank;
 
+//    public Driver() {
+//        this.admin = new Account(88888888, 4444, true);
+//        Account user = new Account(12345678, 1234);
+//        user.setAccountBalance(7000);
+//
+//
+//        this.bank = new ArrayList<>();
+//        this.bank.add(admin);
+//        this.bank.add(user);
+//
+//        new ATM(this.bank);
+//    }
+
+    /** Testing */
     public Driver() {
         this.admin = new Account(88888888, 4444, true);
-        Account user = new Account(12345678, 1234);
-        user.setAccountBalance(7000);
-
 
         this.bank = new ArrayList<>();
         this.bank.add(admin);
-        this.bank.add(user);
+
+        createUsers();
 
         new ATM(this.bank);
+    }
+
+    private void createUsers () {
+        Account jack = new Account(12345678, 1234);
+        jack.setAccountBalance(7000);
+        Account jill = new Account(11223344, 9876);
+        jill.setAccountBalance(500);
+        Account david = new Account(10203040, 1020);
+
+        this.bank.add(jack);
+        this.bank.add(jill);
+        this.bank.add(david);
     }
 
     public static void main(String[] args) {
